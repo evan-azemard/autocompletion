3049a4f4e45b9ad237e16f4757ce115b4d9d1228
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-05-25 13:11:26 +0200

    Page index html,css 100% fait

--- index.php
+++ index.php
@@ -5,8 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    
+<header>
+    <div id="header_div">
+        <h1>Autocomplétion</h1>
+        <form id="header_form" method="get">
+            <input type="search" placeholder="Votre recherche:" aria-label="recherche">
+            <input type="submit" value="Rechercher">
+        </form>
+    </div>
+</header>
+<main id="main_index">
+    <form>
+        <input type="search" id="search_index" placeholder="Votre recherche" aria-label="recherche">
+        <input type="submit" id="recherche_index" value="Rechercher">
+    </form>
+</main>
+<footer>
+    <p>Copyright 2021 © Evan Azemard, Clément Nahmens Autocomplétion | Tous droits réservés</p>
+</footer>
 </body>
 </html>
