eacc2561a5e2f8ae43aca00c2306bf9dd25473d5
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-05-28 16:31:11 +0200

    La suggestion marche en conssole que l'ont soit en majuscule ou minuscule

--- script.js
+++ script.js
@@ -3,33 +3,16 @@ const search =  document.getElementById('search')
 search.addEventListener('keyup', function () {
 
     const val = search.value;
-    console.log(val);
-
-
-    /*   var i = import("data.json")
-       let  u = JSON.parse(i)
-       console.log(u)
-
-       var obj = JSON.parse(localStorage.getItem("data"));
-       console.log(obj);*/
-
-    /*    $.ajax({
-            type: "GET",
-            url: "data.json",
-            dataType: "json",
-            success: function (data) {
-                var res = JSON.parse(data);
-                console.log(res)
-            }});
-        console.log(res);*/
-
 
     $.ajax({
         type: "GET", url: "data.json", dataType: "text",
         success: function (data) {
-            res = JSON.parse(JSON.stringify(data))
+            res = JSON.parse(data)
 
-            console.log(res)
+
+            const result = res.filter(item => item.nom.toLocaleLowerCase().includes(val.toLocaleLowerCase()));
+
+            console.log(result)
 
         }
     })
