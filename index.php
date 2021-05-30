7d360d35457784f212ea40ea8da33a8a8dc21b6f
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-05-30 21:04:11 +0200

    ajout css et rectification des page au code de clem

--- index.php
+++ index.php
@@ -1,32 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<title>Accueil</title>
+	<meta charset="utf-8">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="autocompletion.css">
 </head>
-<body id="body_index">
-<header>
-    <div id="header_div">
-        <h1>Autocomplétion</h1>
-        <form id="header_form" method="get">
-            <input  type="search" id="search" placeholder="Votre recherche:" aria-label="recherche">
-            <input type="submit" value="Rechercher">
-        </form>
-    </div>
-</header>
-<main id="main_index">
-    <form>
-        <input autofocus type="search" id="search_index" placeholder="Votre recherche" aria-label="recherche">
-        <input type="submit" id="recherche_index" value="Rechercher">
-    </form>
-</main>
-<footer>
-    <p>Copyright 2021 © Evan Azemard, Clément Nahmens Autocomplétion | Tous droits réservés</p>
-</footer>
+<body>
+	<main>
+		<section class="searchHome">
+			<div class="img"><img src="sports.png" alt="image de sport"></div>
+			<form action="index.php" method="POST" class="formHome">
+				<input type="search" name="search" class="input" id="searchHome" autocomplete="off">
+				<button type="submit" id="submitHome">rechercher</button>
+			</form>
+		</section>
+	</main>
+	<script type="text/javascript" src="autocompletion.js"></script>
 </body>
 </html>
-<script src="script.js"></script>
--- recherche.php
+++ recherche.php
@@ -1,34 +1,44 @@
+ <?php
+ require "config.php";
+ $recherche = isset($_GET['search']) ? $_GET['search'] : '';
+ if(isset($_GET['search'])){$data = $search->getSearch($recherche);}
+ ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
+	<title>Recherche</title>
+	<meta charset="utf-8">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="fontawesome/all.css">
+	<link rel="stylesheet" type="text/css" href="autocompletion.css">
 </head>
 <body>
 <header>
     <div id="header_div">
         <h1>Autocomplétion</h1>
-        <form id="header_form" method="get">
-            <input type="search" id="search" placeholder="Votre recherche:" aria-label="recherche">
-            <input type="submit" value="Rechercher">
+        <form id="header_form" method="POST">
+            <div id="flfl">
+                <input type="search" name="search"  aria-label="recherche" id="searchNav" autocomplete="off">
+                <div id="fl2">
+                    <div class="autocompletion" id="autocompletion"></div>
+                </div>
+            </div>
+            <input type="submit" id="submitNav" value="Rechercher">
         </form>
-    </div>
+	</div>
 </header>
 <main id="mainmain">
-    <div id="re_div">
-        <span>
-            <a href="#">Lien vers la page Natation</a>
-        </span>
-    </div>
+    <?php if(isset($data)){ ?>
+        <div id="re_div">
+            <span id="searchDisplay" class="searchDisplay">
+                <?php echo $data; ?>
+            </span>
+        </div>
+    <?php } ?>
 </main>
 <footer>
     <p>Copyright 2021 © Evan Azemard, Clément Nahmens Autocomplétion | Tous droits réservés</p>
 </footer>
 </body>
 </html>
-<script src="script.js"></script>
+	<script type="text/javascript" src="autocompletion.js"></script>
